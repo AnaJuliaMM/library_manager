d7d0f68fd4b6246a904b23a2be98b3275b1c0dea
AnaJuliaMM <ana.martins_2004@outlook.com>
2024-05-27 20:26:20 -0300

    feat: create book models

--- library/models/book.py
+++ library/models/book.py
@@ -1,3 +1,16 @@
 from django.db import models
+import uuid
 
-# Create your models here.
+class Book(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    title = models.CharField(max_length=150)
+    author = models.CharField(max_length=150)
+    publisher = models.CharField(max_length=150)
+    gender = models.CharField(max_length=150)
+    publish_date = models.DateField(null=True)
+    checkin_date = models.DateField(auto_now_add=True)
+    is_avaiable = models.BooleanField()
+
+    def __str__(self) -> str:
+        status = "Disponível" if self.is_avaiable else "Reservado"
+        return f"{self.title} escrito por {self.author} (Publicado por {self.publisher} em {self.publish_date}) - {status.upper()}"
