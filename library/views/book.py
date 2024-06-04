6139dc93bd7af10119ce3f16bdb8476899ff9382
AnaJuliaMM <ana.martins_2004@outlook.com>
2024-06-03 22:24:37 -0300

    feat: add template to list books

--- library/views/book.py
+++ library/views/book.py
@@ -6,3 +6,8 @@ from ..serializers.book import BookSerializer
 class BookViewSet (viewsets.ModelViewSet):
     queryset = BookModel.objects.all()
     serializer_class = BookSerializer
+
+    def list(self, request, *args, **kwargs):
+        books = self.queryset
+        return render(request, 'books.html', {'books': books})
+
