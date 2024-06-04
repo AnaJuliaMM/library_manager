bec6db980e688bfcb635c4105652b0f643b0b395
AnaJuliaMM <ana.martins_2004@outlook.com>
2024-06-04 19:22:37 -0300

    feat: direct acervo button  its route

--- library/urls.py
+++ library/urls.py
@@ -3,7 +3,7 @@ from rest_framework.routers import DefaultRouter
 from .views.book import BookViewSet
 
 router = DefaultRouter()
-router.register(r'', BookViewSet)
+router.register(r'', BookViewSet, basename="books")
 
 urlpatterns = [
     path('', include(router.urls)),
