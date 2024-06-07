3ec7192b87e0b7d502968b62a80e2c574794b1ed
AnaJuliaMM <ana.martins_2004@outlook.com>
2024-06-07 20:54:07 -0300

    fix: automatic render new record in book list page

--- library/views/book.py
+++ library/views/book.py
@@ -1,33 +1,35 @@
 from rest_framework import viewsets
+from rest_framework.response import Response
 from django.shortcuts import render, redirect
 from ..models.book import BookModel
 from ..serializers.book import BookSerializer
 from ..forms.create_book import BookForm
+from ..repository import BookRepository
 
-class ListBookView (viewsets.ModelViewSet):
+class ListBookView(viewsets.ModelViewSet):
     queryset = BookModel.objects.all()
     serializer_class = BookSerializer
     http_method_names = ['get']
 
     def list(self, request, *args, **kwargs):
-        books = self.queryset
-        return render(request, 'books.html', {'books': books})
+        books = BookRepository().get_all_books()
+        serialized_books = self.serializer_class(books, many=True)
+        return render(request, 'books.html', {"books": serialized_books.data })
 
-class CreateBookView (viewsets.ModelViewSet):
+class CreateBookView(viewsets.ModelViewSet):
     queryset = BookModel.objects.all()
     serializer_class = BookSerializer
     http_method_names = ['get', 'post']
 
     def list(self, request, *args, **kwargs):
         form = BookForm()
-        return render(request, 'createBook.html', { "form": form})
+        return render(request, 'createBook.html', {"form": form})
     
     def create(self, request, *args, **kwargs):
         form = BookForm(request.POST)
         if form.is_valid():
-          
-            form.save()
+            data = form.cleaned_data
+            BookRepository().create_book(**data)
             return redirect('books-list')
         errors = form.errors
-        print(errors)
         return render(request, 'createBook.html', {'form': form, 'errors': errors})
