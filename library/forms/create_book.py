e10a16b217c53baf3894642b6e076a48f8bf8523
AnaJuliaMM <ana.martins_2004@outlook.com>
2024-06-06 22:33:16 -0300

    feat: customize label and change submit btn value

--- library/forms/create_book.py
+++ library/forms/create_book.py
@@ -5,3 +5,11 @@ class BookForm(forms.ModelForm):
     class Meta:
         model = BookModel
         fields = ['title', 'author', 'publisher', 'gender', 'pages', 'publish_date']
+        labels = {
+                    'title': 'Título',
+                    'author': 'Autor',
+                    'publisher': 'Editora',
+                    'gender': 'Gênero',
+                    'pages': 'Número de Páginas',
+                    'publish_date': 'Data de Publicação',
+                }
