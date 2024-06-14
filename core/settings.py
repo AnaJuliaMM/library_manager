7c20c96bbfcc37edf17f6f472b8cc0bf595e1917
Camila Yukari Yatabe <camila.yatabe@gmail.com>
2024-06-14 11:37:37 -0300

    feat: static settings

--- core/settings.py
+++ core/settings.py
@@ -9,11 +9,22 @@ https://docs.djangoproject.com/en/5.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+# settings.py
+
+# Static files (CSS, JavaScript, Images)
+# settings.py
+
+# Static files (CSS, JavaScript, Images)
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    BASE_DIR / 'user' / 'static',
+]
+
 
 
 # Quick-start development settings - unsuitable for production
