7eca7d13b82706b0b890cceecf82cae7bca1b100
Camila Yukari Yatabe <camila.yatabe@gmail.com>
2024-06-14 11:39:32 -0300

    feat: user serializer create

--- user/serializers/user.py
+++ user/serializers/user.py
@@ -1,7 +1,5 @@
 from rest_framework import serializers
-from ..models.user import UserModel
 
-class UserSerializer(serializers.ModelSerializer):
-    class Meta: 
-        model = UserModel
-        fields =  '__all__'
+class UserLoginSerializer(serializers.Serializer):
+    username = serializers.CharField(max_length=150)
+    password = serializers.CharField(write_only=True)
