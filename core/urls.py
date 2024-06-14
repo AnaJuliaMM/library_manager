dee1b7b0655338557da72fd25f4b2e24526e3d2a
Camila Yukari Yatabe <camila.yatabe@gmail.com>
2024-06-14 20:15:49 -0300

    feat: url account and init token config

--- core/urls.py
+++ core/urls.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework.authtoken import views as authtoken_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('books/', include('library.urls')),
+    # path('api-token-auth/', authtoken_views.obtain_auth_token),
+    path('accounts/', include('django.contrib.auth.urls')),
 ]
