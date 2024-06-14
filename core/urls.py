155fedfbdbc7daf3eed590cc1d04aa22da044c84
Camila Yukari Yatabe <camila.yatabe@gmail.com>
2024-06-14 11:38:53 -0300

    feat: login url

--- core/urls.py
+++ core/urls.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.urls import path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('books/', include('library.urls')),
-    path('users/', include('user.urls')),
     path('user_admin/', include('user_admin.urls')),
+    path('login/', include('user.urls')), 
 ]
