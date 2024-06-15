7565159f9e26db97e5abe81a6344eb28e58a4225
Camila Yukari Yatabe <camila.yatabe@gmail.com>
2024-06-14 21:14:59 -0300

    fix: fix login with customLoginView

--- core/urls.py
+++ core/urls.py
@@ -1,10 +1,13 @@
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework.authtoken import views as authtoken_views
+from user.views.user import CustomLoginView
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('books/', include('library.urls')),
-    # path('api-token-auth/', authtoken_views.obtain_auth_token),
-    path('accounts/', include('django.contrib.auth.urls')),
+    path('api_token_auth/', authtoken_views.obtain_auth_token),
+    # path('accounts/', include('django.contrib.auth.urls')),
+    path('accounts/login/', CustomLoginView.as_view(), name='login'),
 ]
