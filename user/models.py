e87bf97114fdf2dac8c67c7598145de2e2106f89
AnaJuliaMM <ana.martins_2004@outlook.com>
2024-06-17 19:04:16 -0300

    feat: add method to save user entity

--- user/models.py
+++ user/models.py
@@ -5,6 +5,7 @@ class CustomUserManager(BaseUserManager):
     def create_user(self, username, email, password=None, **extra_fields):
         if not email:
             raise ValueError('O campo email é obrigatório')
+        
         email = self.normalize_email(email)
         user = self.model(username=username, email=email, **extra_fields)
         user.set_password(password)
@@ -14,7 +15,9 @@ class CustomUserManager(BaseUserManager):
     def create_superuser(self, username, email, password=None, **extra_fields):
         extra_fields.setdefault('is_staff', True)
         extra_fields.setdefault('is_superuser', True)
-        return self.create_user(username, email, password, **extra_fields)
+        user = self.create_user(username, email, password, **extra_fields)
+        user.save(using=self._db)
+        return user
 
 class CustomUser(AbstractBaseUser, PermissionsMixin):
     id = models.AutoField(primary_key=True)
