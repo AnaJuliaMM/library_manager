2f05946ae66c28a90ba2866d3fd0f524b591de93
AnaJuliaMM <ana.martins_2004@outlook.com>
2024-06-17 19:05:18 -0300

    feat: add logout endpoint

--- user/urls.py
+++ user/urls.py
@@ -5,7 +5,8 @@ from .views import *
 urlpatterns = [
     path(r'', ListUserView.as_view(),  name="users-list" ),
     path(r'create/', CreateUserView.as_view(),  name="create-user" ),
-    path('login/', CustomLoginView.as_view(), name='user-login'),
+    path('login/', LoginView.as_view(), name='user-login'),
+    path('logout/', LogoutView.as_view(), name='user-logout'),
     # path(r'delete/<str:pk>', DeleteBookView.as_view(),  name="delete-book" ),
     # path(r'update/<str:pk>', EditBookView.as_view(),  name="update-book" )
 ]
